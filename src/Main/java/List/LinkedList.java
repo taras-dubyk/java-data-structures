08bd869cd76543a8f5aeb07e687ec9b619d67b0c
taras d <tarass.dubyk@gmail.com>
2017-02-16 15:36:45 +0200

    Added Node inner class. Changed logic for size calculation for best performance.

--- src/Main/java/List/LinkedList.java
+++ src/Main/java/List/LinkedList.java
@@ -3,19 +3,30 @@
  */
 public class LinkedList<T> implements IList<T> {
     private Node<T> head = null;
+    private int size = 0;
+
+    private static class Node<T> {
+        private Node<T> next = null;
+        T data;
+
+        private Node(T data) {
+            this.data = data;
+        }
+    }
 
     @Override
     public void add(T value) {
         if(head == null){
-            head = new Node(value);
+            head = new Node<>(value);
         }
         else {
             Node<T> tail = head;
             while (tail.next != null){
                 tail = tail.next;
             }
-            tail.next = new Node(value);
+            tail.next = new Node<>(value);
         }
+        size++;
     }
 
     @Override
@@ -23,7 +34,7 @@ public class LinkedList<T> implements IList<T> {
         if(!isPositionCorrect(position))
             throw new ArrayIndexOutOfBoundsException(position);
         if(position == 0){
-            Node<T> insert = new Node(value);
+            Node<T> insert = new Node<T>(value);
             insert.next = head;
             head = insert;
         }
@@ -32,9 +43,10 @@ public class LinkedList<T> implements IList<T> {
         while (count < position - 1){
             previous = previous.next;
         }
-        Node<T> insert = new Node(value);
+        Node<T> insert = new Node<>(value);
         insert.next = previous.next;
         previous.next = insert;
+        size++;
     }
 
     @Override
@@ -44,8 +56,10 @@ public class LinkedList<T> implements IList<T> {
         Node previous = getElementOnCurrentPosition(position - 1);
         Node current = previous.next;
         previous.next = current.next;
+        size--;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public T get(int position) {
         if(!isPositionCorrect(position))
@@ -65,11 +79,12 @@ public class LinkedList<T> implements IList<T> {
     @Override
     public void clear() {
         head = null;
+        size = 0;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public T[] toArray() {
-        int size = size();
         T[] data = (T[]) new Object[size];
         for(int i=0; i<size; i++){
             data[i] = (T) getElementOnCurrentPosition(i).data;
@@ -79,19 +94,11 @@ public class LinkedList<T> implements IList<T> {
 
     @Override
     public int size() {
-        if (head == null)
-            return 0;
-        int size = 1;
-        Node<T> temp = head;
-        while(temp.next != null){
-            size++;
-            temp = temp.next;
-        }
         return size;
     }
 
     private Boolean isPositionCorrect(int position){
-        return !(position < 0 || position >= size());
+        return !(position < 0 || position >= size);
     }
 
     private Node getElementOnCurrentPosition(int position){
